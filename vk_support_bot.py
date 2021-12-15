357b33b6e9918285c8acc44d62e081d5633907c4
n1k0din <nik726@gmail.com>
2021-12-15 21:13:39 +0700

    Add DialogFlow to VK bot

--- vk_support_bot.py
+++ vk_support_bot.py
@@ -2,13 +2,23 @@ import random
 
 import vk_api as vk
 from environs import Env
-from vk_api.longpoll import VkEventType, VkLongPoll
+from vk_api.longpoll import Event, VkEventType, VkLongPoll
+from vk_api.vk_api import VkApiMethod
+
+from dialogflow import get_dialogflow_answer
 
 
-def echo(event, vk_api):
+def answer(dialogflow_project_id: str, event: Event, vk_api: VkApiMethod) -> None:
+    """Answer the user message."""
+    answer_text = get_dialogflow_answer(
+        project_id=dialogflow_project_id,
+        session_id=str(event.user_id),
+        text=event.text,
+    )
+
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=answer_text,
         random_id=random.randint(1, 1000),
     )
 
@@ -18,6 +28,7 @@ if __name__ == '__main__':
     env.read_env()
 
     vk_token = env('VK_TOKEN')
+    dialogflow_project_id = env('DIALOGFLOW_PROJECT_ID')
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
 
@@ -25,4 +36,4 @@ if __name__ == '__main__':
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            answer(dialogflow_project_id, event, vk_api)
